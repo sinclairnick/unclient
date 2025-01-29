f42680fdc664462ea1ed67061b842b4868b3ea1e
Nick Sinclair <nicksinclair@xtra.co.nz>
2025-01-29 17:31:41 +1300

    Fix sub exports

---
+++ fetch.js
@@ -0,0 +1,1 @@
+module.exports = require("./dist/fetch.js");
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,8 +1,5 @@
 import { defineConfig } from "tsup";
-import pkg from "./package.json";
-import fs from "node:fs/promises";
-
-const Exports = ["axios", "fetch"];
+import { subExports } from "esbuild-sub-exports";
 
 export default defineConfig({
   entry: {
@@ -16,31 +13,5 @@ export default defineConfig({
   outDir: "dist",
   sourcemap: false,
   treeshake: "recommended",
-  async onSuccess() {
-    const files = new Set(pkg.files);
-
-    for (const ex of Exports) {
-      files.add(`dist/${ex}.mjs`);
-      files.add(`dist/${ex}.js`);
-      files.add(`dist/${ex}.d.ts`);
-
-      pkg.exports[`./${ex}`] = {
-        types: `./dist/${ex}.d.ts`,
-        import: `./dist/${ex}.mjs`,
-        require: `./dist/${ex}.js`,
-      };
-    }
-
-    await fs.writeFile(
-      "./package.json",
-      JSON.stringify(
-        {
-          ...pkg,
-          files: Array.from(files),
-        },
-        null,
-        2
-      )
-    );
-  },
+  esbuildPlugins: [subExports({ entries: ["axios", "fetch"] })],
 });
